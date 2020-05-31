93902f68db7753639f3c15086358676d42a03f86
japhheth <japhheth@gmail.com>
2020-05-31 01:59:15 +0100

    comit

---
+++ Assets/Js/main.js
@@ -0,0 +1,7 @@
+
+
+document.body.classList.add("no-transactions");
+setTimeout(function(){
+    document.body.querySelector('#preloader').classList.add('hide');
+    document.body.classList.remove("no-transactions")
+}, 4000)
